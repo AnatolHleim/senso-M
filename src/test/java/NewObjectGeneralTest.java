18c6e4cceb4dfb1364965a281def975d8c5fe342
Anatol Hleim <anatolhleim@gmail.com>
2019-06-18 11:52:10 +0300

    testOne

--- src/test/java/NewObjectGeneralTest.java
+++ src/test/java/NewObjectGeneralTest.java
@@ -8,36 +8,29 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class NewObjectGeneralTest extends TestData{
-    @Test(description = "newObject")
-    public void newObject(){
+    @Test
+    public void one(){
+        driver.get("http://localhost:4200/");
+        WebElement ownerInput =driver.findElement(By.xpath("//div[@class='input datalist' and preceding-sibling::div/h2[text()='Владелец процесса']]"));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        ownerInput.click();
+        WebElement input = ownerInput.findElement(By.xpath("./input"));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        input.sendKeys("2");
+        List<WebElement> listSelectOwner = ownerInput.findElements(By.xpath("./datalist/option"));
 
-
-        NewObjectPage newObjectPage = new NewObjectPage(driver, log);
-
-
-        newObjectPage.openPage();
-            driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-            newObjectPage.click(newObjectPage.ownerInput);
-//        WebElement my_dropdown = driver.findElement(By.xpath("//*[@id=\"names\"]"));
-//
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//*[@class='input datalist']//option[1]")).click();
-      // Select select = new (driver.findElement(By.xpath("//*[@id=\"names\"]")));
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-      //  select.selectByVisibleText("Сергей Сергеевич Иванов 2");
-        //driver.findElement(By.xpath("//*[@id=\"system-main\"]/div/table/tbody/tr[1]/td[2]/select/option[3]")).click();
-           //newObjectPage.click(driver.findElement(By.xpath("//*[@id=\"names\"]")));
+        for (WebElement e:listSelectOwner
+        ) {
+            System.out.println(e.getAttribute("value"));
+        }
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //   newObjectPage.type("fake",newObjectPage.ownerInput);
-
-        Assert.assertTrue(true);
-
     }
 }
